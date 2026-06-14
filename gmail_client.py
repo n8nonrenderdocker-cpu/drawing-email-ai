ff7f0489ea4351c5ece13161228388de78847899
n8nonrenderdocker-cpu <lenlenalmodal@gmail.com>
2026-06-14 21:11:30 +0800

    Update gmail_client.py

--- gmail_client.py
+++ gmail_client.py
@@ -12,8 +12,8 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-from .config import Settings
-from .models import EmailAttachment, EmailContext
+from config import Settings
+from models import EmailAttachment, EmailContext
 
 
 LOGGER = logging.getLogger(__name__)
